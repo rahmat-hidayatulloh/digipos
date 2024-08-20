9f8bc5a8c66b7b5c2f76e2c87b497646f2b6c684
rahmat.h <rahmat.hidayatulloh2010@gmail.com>
2024-08-21 02:45:20 +0700

    msg: Master Commit

---
+++ digipos/src/main/java/id/co/digipos/DigiposApplication.java
@@ -0,0 +1,13 @@
+package id.co.digipos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiposApplication.class, args);
+	}
+
+}
